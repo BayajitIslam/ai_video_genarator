34101f6919164b7883f9c9550ae48787de7dbf58
Bayajit Islam <realbayajitislam@gmail.com>
2026-02-09 15:35:38 +0600

    drawer design

--- lib/core/constant/app_icons.dart
+++ lib/core/constant/app_icons.dart
@@ -8,4 +8,10 @@ class AppIcons {
   static const String genarate = 'assets/icons/navbar/generate_icon.svg';
   static const String profile = 'assets/icons/navbar/profile_icon.svg';
   static const String video = 'assets/icons/navbar/videos_icon.svg';
+
+  //Drawer
+  static const String person = 'assets/icons/drawer/person.svg';
+  static const String privacyPolicy = 'assets/icons/drawer/privacy policy.svg';
+  static const String subscription = 'assets/icons/drawer/subscription.svg';
+  static const String logout = 'assets/icons/drawer/logout.svg';
 }
--- lib/features/home/presentation/widgets/my_drawer.dart
+++ lib/features/home/presentation/widgets/my_drawer.dart
@@ -1,10 +1,54 @@
+import 'package:ai_video_genarator/core/constant/app_icons.dart';
+import 'package:ai_video_genarator/core/theme/app_pallete.dart';
+import 'package:ai_video_genarator/core/utils/screen_size.dart';
 import 'package:flutter/material.dart';
+import 'package:flutter_svg/svg.dart';
 
 class MyDrawer extends StatelessWidget {
   const MyDrawer({super.key});
 
   @override
   Widget build(BuildContext context) {
-    return Drawer();
+    return Drawer(
+      backgroundColor: AppPallete.blackGradient4,
+      child: SafeArea(
+        child: ListView(
+          padding: EdgeInsets.zero,
+          children: [
+            Align(
+              alignment: AlignmentGeometry.centerLeft,
+              child: IconButton(
+                onPressed: () {
+                  Navigator.pop(context);
+                },
+                icon: const Icon(Icons.menu),
+              ),
+            ),
+            SizedBox(height: context.spacing32),
+            ListTile(
+              leading: SvgPicture.asset(AppIcons.person),
+              title: const Text('Profile'),
+              onTap: () {
+                // Handle item 1 tap
+              },
+            ),
+            ListTile(
+              leading: SvgPicture.asset(AppIcons.subscription),
+              title: const Text('Subscription'),
+              onTap: () {
+                // Handle item 2 tap
+              },
+            ),
+            ListTile(
+              leading: SvgPicture.asset(AppIcons.privacyPolicy),
+              title: const Text('Privacy policy'),
+              onTap: () {
+                //Handle item 3 tap
+              },
+            ),
+          ],
+        ),
+      ),
+    );
   }
 }
